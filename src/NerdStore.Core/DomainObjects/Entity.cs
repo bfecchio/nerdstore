49038dfa7dc3f26647c19767c931342a679f44c9
bfecchio <bruno.fecchio@gmail.com>
2020-12-03 22:15:31 -0300

    feat: commit inicial de criacao da estrutura base do projeto

---
+++ src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        #region Public Properties
+
+        public Guid Id { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        #endregion
+
+        #region Overriden Methods
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+
+        #endregion
+
+        #region Operators Methods
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null)) return true;
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null)) return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        #endregion
+    }
+}
