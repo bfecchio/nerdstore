c73fa69b800106df210de4dd6e4cd9b17c7604c3
bfecchio <bruno.fecchio@gmail.com>
2020-12-04 19:48:22 -0300

    feat: implementacao de DomainServices

---
+++ src/NerdStore.Catalogo.Domain/Services/IEstoqueService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain.Services
+{
+    public interface IEstoqueService : IDisposable
+    {
+        #region IEstoqueService Members
+
+        Task<bool> DebitarEstoque(Guid produtoId, int quantidade);
+        Task<bool> ReporEstoque(Guid produtoId, int quantidade);
+
+        #endregion
+    }
+}
